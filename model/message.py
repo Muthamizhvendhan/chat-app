6d27ba2e1970bc53510c709cfdc38f0b2efae5dc
Muthamizh <muthamizhvendhan66@gmail.com>
2025-07-22 16:25:48 +0530

    Begin message model developement

--- model/message.py
+++ model/message.py
@@ -1,1 +1,20 @@
-message.py
+#Message Main model
+class message(object):
+
+  #Initializer.
+  def __init__(self):
+    pass
+
+  CRUD => CREATE READ UPDATE DELETE
+  # Create message.
+  def create_message(self):
+    pass
+  # Send message.
+  def send_message(self):
+    pass
+  # Received message.
+  def receive_message(self):
+    pass
+  # Delete message.
+  def delete_message(self):
+    pass
