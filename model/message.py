22d9761b711279a671ee1d2316ad5f522c4735de
Muthamizh Vendhan <113575464+Muthamizhvendhan@users.noreply.github.com>
2025-07-23 05:51:45 +0530

    Requesting code merge to staging branch (#1)
    * Begin message web controller development
    
    * Begin message model developement
    
    * Begin message model developement

---
+++ model/message.py
@@ -0,0 +1,21 @@
+#Message Main model
+class message(object):
+
+  #Initializer.
+  def __init__(self):
+    pass
+
+  CRUD => CREATE READ UPDATE DELETE
+  # Create message.
+  def create_message(self):
+    pass
+  # Send message.
+  def send_message(self):
+    pass
+  # Received message.
+  def receive_message(self):
+    pass
+  # Delete message.
+  def delete_message(self):
+    pass
+    
